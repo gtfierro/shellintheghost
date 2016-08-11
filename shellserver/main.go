e9584991fc502b1447db200386a3584f6b6a91a4
Gabe Fierro <gtfierro225@gmail.com>
2016-08-11 16:00:26 -0700

    switch to command line input

--- shellserver/main.go
+++ shellserver/main.go
@@ -4,14 +4,18 @@ import (
 	"github.com/gtfierro/shellintheghost/server"
 	bw2 "gopkg.in/immesys/bw2bind.v5"
 	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: ./shellserver <base svc uri>")
+	}
 	client := bw2.ConnectOrExit("")
 	vk := client.SetEntityFromEnvironOrExit()
 	client.OverrideAutoChainTo(true)
 
-	server := server.NewServerService(client, vk, "gabe.pantry/terminals", "/bin/bash")
+	server := server.NewServerService(client, vk, os.Args[1], "/bin/bash")
 	err := server.AddTerminal("0")
 	if err != nil {
 		log.Fatal(err)
